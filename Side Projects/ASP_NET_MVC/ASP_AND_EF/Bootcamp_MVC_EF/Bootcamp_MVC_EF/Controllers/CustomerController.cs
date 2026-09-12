f1186bfec1d9e1faf8ad16659b2ffba7e5b77c61
almasouda982 <Azizmohammed9822@gmail.com>
2026-09-12 14:35:17 +0300

    controller method

--- Side Projects/ASP_NET_MVC/ASP_AND_EF/Bootcamp_MVC_EF/Bootcamp_MVC_EF/Controllers/CustomerController.cs
+++ Side Projects/ASP_NET_MVC/ASP_AND_EF/Bootcamp_MVC_EF/Bootcamp_MVC_EF/Controllers/CustomerController.cs
@@ -12,21 +12,12 @@ namespace Bootcamp_MVC_EF.Controllers
         {
             _db = db;
         }
-        public IActionResult GetCustomers()
-        {
-            IEnumerable<Customer> customers = _db.Customers.ToList();
-            return Ok(customers);
-        }
-
         public IActionResult Index()
         {
-            IList<Customer> customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Doe", Email = "John@Doe.com", Phone = "123-456-7890" },
-                new Customer { Id = 2, Name = "Jane Smith", Email = "Jane@Smith.com", Phone = "987-654-3210" },
-                new Customer { Id = 3, Name = "Bob Johnson", Email = "Bob Johnson.com", Phone = "555-555-5555" },
-                };
+            IEnumerable<Customer> customers = _db.Customers.ToList();
             return View(customers);
         }
+
+
     }
 }
