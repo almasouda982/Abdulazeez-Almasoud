b7db5aecf86a24a0e65b48addb879fbbff713262
almasouda98 <Azizmohammed9822@gmail.com>
2026-09-14 22:10:44 +0300

    Complete the first CRUD for Enrollment

---
+++ Main Projects/Project6_ASP_NET_MVC/University/University/Controllers/EnrollmentsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.Data;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class EnrollmentsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public EnrollmentsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Enrollment> enrollments = _db.Enrollments.ToList();
+            return View(enrollments);
+        }
+
+        //==============================
+        // Create
+        //==============================
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Enrollment enrollment)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Please fill in all required fields.");
+                return View(enrollment);
+            }
+            _db.Enrollments.Add(enrollment);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //==============================
+        // Edit
+        //==============================
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var enr = _db.Enrollments.Find(Id);
+            if (enr == null)
+            {
+                return NotFound();
+            }
+            return View(enr);
+        }
+        [HttpPost]
+        public IActionResult Edit(Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Enrollments.Update(enrollment);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Please fill all the required fields.");
+            return View(enrollment);
+        }
+
+        //===========
+        // Delete
+        //===========
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var enr = _db.Enrollments.Find(Id);
+            if (enr == null)
+            {
+                return NotFound();
+            }
+            return View(enr);
+        }
+        [HttpPost]
+        public IActionResult Delete(Enrollment enrollment)
+        {
+            _db.Enrollments.Remove(enrollment);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Main Projects/Project6_ASP_NET_MVC/University/University/Models/Enrollment.cs
+++ Main Projects/Project6_ASP_NET_MVC/University/University/Models/Enrollment.cs
@@ -4,10 +4,16 @@ namespace University.Models
 {
     public class Enrollment
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public int StudentId { get; set; }
+        [Required]
         public string FinalGrade { get; set; } = "";
+        [Required]
+        //change to bool
         public string Status { get; set; } = "";
+        [Required]
         public decimal LetterGradePoints { get; set; } = 0;
 
 
