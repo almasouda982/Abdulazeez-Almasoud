daa2f07b4c91b033c1797565f182d5407ed9bed7
almasouda98 <Azizmohammed9822@gmail.com>
2026-09-14 21:44:48 +0300

    Complete the first CRUD for CourseSection

---
+++ Main Projects/Project6_ASP_NET_MVC/University/University/Controllers/CourseSectionsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.Data;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class CourseSectionsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CourseSectionsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CourseSection> courseSections = _db.CourseSections.ToList();
+            return View(courseSections);
+        }
+
+        //==============================
+        // Create
+        //==============================
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CourseSection courseSection)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Please fill in all required fields.");
+                return View(courseSection);
+            }
+            _db.CourseSections.Add(courseSection);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //==============================
+        // Edit
+        //==============================
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var crs = _db.CourseSections.Find(Id);
+            if (crs == null)
+            {
+                return NotFound();
+            }
+            return View(crs);
+        }
+        [HttpPost]
+        public IActionResult Edit(CourseSection courseSection)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.CourseSections.Update(courseSection);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Please fill all the required fields.");
+            return View(courseSection);
+        }
+
+        //===========
+        // Delete
+        //===========
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var crs = _db.CourseSections.Find(Id);
+            if (crs == null)
+            {
+                return NotFound();
+            }
+            return View(crs);
+        }
+        [HttpPost]
+        public IActionResult Delete(CourseSection courseSection)
+        {
+            _db.CourseSections.Remove(courseSection);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Main Projects/Project6_ASP_NET_MVC/University/University/Models/CourseSection.cs
+++ Main Projects/Project6_ASP_NET_MVC/University/University/Models/CourseSection.cs
@@ -1,12 +1,20 @@
-﻿namespace University.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University.Models
 {
     public class CourseSection
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public int SectionNumber { get; set; }
+        [Required]
         public int MaxCapacity { get; set; }
+        [Required]
         public string DaysOfWeek { get; set; } = "";
+        [Required]
         public string TimeSlot { get; set; } = "";
+        [Required]
         public  int CourseId { get; set; }
 
     }
