d12e4648f294156e7b8b4daa968ac25401dc20e4
almasouda98 <Azizmohammed9822@gmail.com>
2026-09-14 21:13:55 +0300

    Complete the first CRUD for DegreeRequirement Table after modding some models

---
+++ Main Projects/Project6_ASP_NET_MVC/University/University/Controllers/DegreeRequirementsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.Data;
+using University.Models;
+
+namespace University.Controllers
+{
+    public class DegreeRequirementsController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public DegreeRequirementsController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<DegreeRequirement> degreeRequirements = _db.DegreeRequirements.ToList();
+            return View(degreeRequirements);
+        }
+
+        //==============================
+        // Create
+        //==============================
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(DegreeRequirement degreeRequirement)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Please fill in all required fields.");
+                return View(degreeRequirement);
+            }
+            _db.DegreeRequirements.Add(degreeRequirement);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //==============================
+        // Edit
+        //==============================
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var degr = _db.DegreeRequirements.Find(Id);
+            if (degr == null)
+            {
+                return NotFound();
+            }
+            return View(degr);
+        }
+        [HttpPost]
+        public IActionResult Edit(DegreeRequirement degreeRequirement)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.DegreeRequirements.Update(degreeRequirement);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Please fill all the required fields.");
+            return View(degreeRequirement);
+        }
+
+        //===========
+        // Delete
+        //===========
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var degr = _db.DegreeRequirements.Find(Id);
+            if (degr == null)
+            {
+                return NotFound();
+            }
+            return View(degr);
+        }
+        [HttpPost]
+        public IActionResult Delete(DegreeRequirement degreeRequirement)
+        {
+            _db.DegreeRequirements.Remove(degreeRequirement);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Main Projects/Project6_ASP_NET_MVC/University/University/Models/DegreeRequirement.cs
+++ Main Projects/Project6_ASP_NET_MVC/University/University/Models/DegreeRequirement.cs
@@ -12,9 +12,11 @@ namespace University.Models
         public string CategoryName { get; set; } = "";
         [Required]
         [DisplayName("Required Credits")]
+        [Range(40,80, ErrorMessage ="Required credits must be betwen 40 and 80 credits")]
         public int RequiredCredits { get; set; } = 0;
         [Required]
         [DisplayName("Minimum Level")]
+        [RegularExpression(@"^(200|300|400)$", ErrorMessage = "Value must be either 200, 300, or 400.")]
         public string MinLevel { get; set; } = "";
     }
 }
